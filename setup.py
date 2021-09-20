4642f6f12fa58fa894b3d0d22262a00353d13f37
dborrelli <18274487+dborrelli@users.noreply.github.com>
2021-09-19 20:35:33 -0400

    Add setup.py file

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import setuptools
 
 setuptools.setup(
     name="chatintents",
-    version="0.0.1",
+    version="0.1",
     packages=setuptools.find_packages(exclude=['data', 'images', 'notebooks']),
 )
